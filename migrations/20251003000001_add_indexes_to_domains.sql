09e14214a92fc2b1aeabee5645dc038557646499
zuxt268 <y.ikezawa93@gmail.com>
2025-10-03 19:31:02 +0900

    mod

--- migrations/20251003000001_add_indexes_to_domains.sql
+++ migrations/20251003000001_add_indexes_to_domains.sql
@@ -5,7 +5,6 @@ CREATE INDEX idx_domains_industry ON domains(industry);
 CREATE INDEX idx_domains_owner ON domains(owner_id);
 CREATE INDEX idx_domains_ssl ON domains(is_ssl);
 CREATE INDEX idx_domains_can_view ON domains(can_view);
-CREATE INDEX idx_domains_is_japan ON domains(is_japan);
 CREATE INDEX idx_domains_is_send ON domains(is_send);
 CREATE INDEX idx_domains_owner_id ON domains(owner_id);
 
@@ -16,6 +15,5 @@ DROP INDEX idx_domains_industry ON domains;
 DROP INDEX idx_domains_owner ON domains;
 DROP INDEX idx_domains_ssl ON domains;
 DROP INDEX idx_domains_can_view ON domains;
-DROP INDEX idx_domains_is_japan ON domains;
 DROP INDEX idx_domains_is_send ON domains;
 DROP INDEX idx_domains_owner_id ON domains;
