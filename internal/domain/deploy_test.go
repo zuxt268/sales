fc9e2c5cb4ea17cd9c2d79d87623b7feeec5b4db
zuxt268 <y.ikezawa93@gmail.com>
2025-10-29 13:56:49 +0900

    mod

--- internal/domain/deploy_test.go
+++ internal/domain/deploy_test.go
@@ -11,16 +11,18 @@ func TestDeploy(t *testing.T) {
 			Domain:   "base2.hp-standard.com",
 			ServerID: "xb932770",
 		},
-		Dst: Deploy{
-			Domain:   "b028efa8.hp-standard.net",
-			ServerID: "xb157298",
+		Dst: []Deploy{
+			{
+				Domain:   "b028efa8.hp-standard.net",
+				ServerID: "xb157298",
+			},
 		},
 	}
 	fmt.Println(req.Src.WordpressRootDirectory())
 	fmt.Println(req.Src.SecretConfigPath())
 	fmt.Println(req.Src.MuPluginDirectory())
 
-	fmt.Println(req.Dst.WordpressRootDirectory())
-	fmt.Println(req.Dst.SecretConfigPath())
-	fmt.Println(req.Dst.MuPluginDirectory())
+	fmt.Println(req.Dst[0].WordpressRootDirectory())
+	fmt.Println(req.Dst[0].SecretConfigPath())
+	fmt.Println(req.Dst[0].MuPluginDirectory())
 }
