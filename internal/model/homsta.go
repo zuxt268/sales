f536183e864267ff908a8ecd5d3e12fc744bf72d
zuxt268 <y.ikezawa93@gmail.com>
2026-01-09 23:12:32 +0900

    mod

--- internal/model/homsta.go
+++ internal/model/homsta.go
@@ -5,19 +5,19 @@ import (
 )
 
 type Homsta struct {
-	ID          int       `gorm:"column:id;primaryKey;autoIncrement"`
-	Domain      string    `gorm:"column:domain"`
-	BlogName    string    `gorm:"column:blog_name"`
-	Path        string    `gorm:"column:path"`
-	SiteURL     string    `gorm:"column:site_url"`
-	Description string    `gorm:"column:description"`
-	DBName      string    `gorm:"column:db_name"`
-	Users       string    `gorm:"column:users"`
-	DBUsage     string    `gorm:"column:db_usage"`
-	DiscUsage   string    `gorm:"column:disc_usage"`
-	Industry    string    `gorm:"column:industry"`
-	UpdatedAt   time.Time `gorm:"column:updated_at;autoUpdateTime"`
-	CreatedAt   time.Time `gorm:"column:created_at;autoCreateTime"`
+	ID          int        `gorm:"column:id;primaryKey;autoIncrement"`
+	Domain      string     `gorm:"column:domain"`
+	BlogName    string     `gorm:"column:blog_name"`
+	Path        string     `gorm:"column:path"`
+	SiteURL     string     `gorm:"column:site_url"`
+	Description string     `gorm:"column:description"`
+	DBName      string     `gorm:"column:db_name"`
+	Users       string     `gorm:"column:users"`
+	DBUsage     string     `gorm:"column:db_usage"`
+	DiscUsage   string     `gorm:"column:disc_usage"`
+	Industry    string     `gorm:"column:industry"`
+	UpdatedAt   *time.Time `gorm:"column:updated_at;autoUpdateTime"`
+	CreatedAt   *time.Time `gorm:"column:created_at;autoCreateTime"`
 }
 
 func (Homsta) TableName() string {
